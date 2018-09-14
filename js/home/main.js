dec3cd3dd7707c094a2f6775020a2ddd77beb07d
= <akashaaron1998@gmail.com>
2018-09-14 18:53:51 +0530

    Boilderplate final

---
+++ js/home/main.js
@@ -0,0 +1,23 @@
+var init = function () {
+  var isMobile = navigator.userAgent &&
+    navigator.userAgent.toLowerCase().indexOf('mobile') >= 0;
+  var isSmall = window.innerWidth < 1000;
+
+  var ps = new Particle({
+    ptlGap: isMobile || isSmall ? 3 : 5,
+    ptlSize: isMobile || isSmall ? 3 : 3,
+    width: 1e9,
+    height: 1e9,
+    mouseForce: 5000
+  });
+
+  (window.addEventListener ?
+    window.addEventListener('click', function () {
+      ps.init(true)
+    }, false) :
+    window.onclick = function () {
+      ps.init(true)
+    });
+}
+
+init()
