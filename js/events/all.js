1bfa9694a5bb9d34446de4d62523ec863e81abac
Apoorv Agarwal <apoorv.169105045@muj.manipal.edu>
2018-09-17 22:17:11 +0530

    Added campus ambassador page and proto team page for web and graphics

--- js/events/all.js
+++ js/events/all.js
@@ -1,7 +1,9 @@
-const sword = $('.nav ul li')
+const sword = $('.nav ul li span')
 
 $(document).ready(() => {
   $('#fullpage').fullpage({
+    scrollBar: true,
+
     anchors: [
       "club-aperture",
       "focus-desc", "focus-info", "focus-registration",
@@ -11,6 +13,7 @@ $(document).ready(() => {
       "pictureperfect-desc", "pictureperfect-info", "pictureperfect-registration",
       "powershoot-desc", "powershoot-info", "powershoot-registration",
 
+      "club-tmc",
       "octaves-desc", 'octaves-info', 'octaves-registration',
       "dhwani-desc", 'dhwani-info', 'dhwani-registration',
       "saptak-desc", 'saptak-info', 'saptak-registration',
@@ -19,6 +22,12 @@ $(document).ready(() => {
       "beatstreet-desc", 'beatstreet-info', 'beatstreet-registration',
       "woodstock-desc", 'woodstock-info', 'woodstock-registration',
 
+      "club-coreografia",
+      "nextar-desc", 'nextar-info', 'nextar-registration',
+      "showcase-desc", 'showcase-info', 'showcase-registration',
+      "groundzero-desc", 'groundzero-info', 'groundzero-registration',
+      "stepsvsbeats-desc", 'stepsvsbeats-info', 'stepsvsbeats-registration',
+
     ],
     afterLoad: (origin, dest) => {
       let option = 0
@@ -26,9 +35,8 @@ $(document).ready(() => {
       $('#wordmark').css({
         filter: `invert(${option})`
       })
-      if (dest.anchor.split('-')[0] !== 'club') sword.css({
-        filter: `invert(${option})`
-      })
+      if (dest.anchor.split('-')[0] !== 'club') sword.addClass('nav-darken')
+      else sword.removeClass('nav-darken')
     }
   })
 })
