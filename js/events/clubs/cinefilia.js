ced70c8c2a292a95914f01abdb3bc58867d9149a
Parth <abhishek.a.pandey98@gmail.com>
2018-09-19 03:24:54 +0530

    Cinefilia + Aperture Added

--- js/events/clubs/cinefilia.js
+++ js/events/clubs/cinefilia.js
@@ -7,15 +7,12 @@ $(document).ready(() => {
     anchors: [
       "club-cinefilia",
       "admak-desc",'admak-info','admak-rules','admak-registration',
-      "awaaz-desc",'awaaz-info','awaaz-rules','awaaz-registration',
       "humorus-desc",'humorus-info','humorus-rules','humorus-registration',
       "missionimprovable-desc",'missionimprovable-info','missionimprovable-rules','missionimprovable-registration',
       "monoact-desc",'monoact-info','monoact-rules','monoact-registration',
       "pandorasbox-desc",'pandorasbox-info','pandorasbox-rules','pandorasbox-registration',
       "rangmanch-desc",'rangmanch-info','rangmanch-rules','rangmanch-registration',
       "spoofydo-desc",'spoofydo-info','spoofydo-rules','spoofydo-registration'
-
-
     ],
     afterLoad: (origin, dest) => {
       let option = 0;
