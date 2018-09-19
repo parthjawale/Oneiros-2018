0e318d0ecd9bec06e887d5ffe052191a8f346a51
Apoorv Agarwal <apoorv.169105045@muj.manipal.edu>
2018-09-20 03:32:31 +0530

    major changes

--- js/common/btn.js
+++ js/common/btn.js
@@ -1,19 +1,29 @@
-$(".home-splash").mousemove(e => {
-  const x = e.offsetX;
-  const y = e.offsetY;
-  const circle = $(".home-splash .ono-animatedbtn__circle");
+$('.home-splash').mousemove(e => {
+  const x = e.offsetX
+  const y = e.offsetY
+  const circle = $('.home-splash .ono-animatedbtn__circle')
   circle.css({
     left: `${x / (window.innerWidth / 100)}px`,
     top: `${y / 40}px`
-  });
-});
+  })
+})
 
-$(".home-about").mousemove(e => {
-  const x = e.offsetX;
-  const y = e.offsetY;
-  const circle = $(".home-about .ono-animatedbtn__circle");
+$('.home-about').mousemove(e => {
+  const x = e.offsetX
+  const y = e.offsetY
+  const circle = $('.home-about .ono-animatedbtn__circle')
   circle.css({
     left: `${x / (window.innerWidth / 100)}px`,
     top: `${y / 40}px`
-  });
-});
+  })
+})
+
+$('body').mousemove(e => {
+  const x = e.offsetX
+  const y = e.offsetY
+  const circle = $('.events-list > .ono-animatedbtn__circle')
+  circle.css({
+    left: `${x / (window.innerWidth / 100)}px`,
+    top: `${y / 40}px`
+  })
+})
