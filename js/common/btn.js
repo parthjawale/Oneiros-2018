44e0ca3f81960edefe15f987d93b9b758adb7fdf
Apoorv Agarwal <apoorv.169105045@muj.manipal.edu>
2018-09-20 03:58:57 +0530

    made little improvements to the events list

--- js/common/btn.js
+++ js/common/btn.js
@@ -1,29 +1,38 @@
-$('.home-splash').mousemove(e => {
-  const x = e.offsetX
-  const y = e.offsetY
-  const circle = $('.home-splash .ono-animatedbtn__circle')
-  circle.css({
-    left: `${x / (window.innerWidth / 100)}px`,
-    top: `${y / 40}px`
+$(document).ready(function() {
+  const isSmall = window.innerWidth < 500
+  $('.home-splash').mousemove(e => {
+    const x = e.offsetX
+    const y = e.offsetY
+    const circle = $('.home-splash .ono-animatedbtn__circle')
+    if (!isSmall) {
+      circle.css({
+        left: `${x / (window.innerWidth / 100)}px`,
+        top: `${y / 40}px`
+      })
+    }
   })
-})
 
-$('.home-about').mousemove(e => {
-  const x = e.offsetX
-  const y = e.offsetY
-  const circle = $('.home-about .ono-animatedbtn__circle')
-  circle.css({
-    left: `${x / (window.innerWidth / 100)}px`,
-    top: `${y / 40}px`
+  $('.home-about').mousemove(e => {
+    const x = e.offsetX
+    const y = e.offsetY
+    const circle = $('.home-about .ono-animatedbtn__circle')
+    if (!isSmall) {
+      circle.css({
+        left: `${x / (window.innerWidth / 100)}px`,
+        top: `${y / 40}px`
+      })
+    }
   })
-})
 
-$('body').mousemove(e => {
-  const x = e.offsetX
-  const y = e.offsetY
-  const circle = $('.events-list > .ono-animatedbtn__circle')
-  circle.css({
-    left: `${x / (window.innerWidth / 100)}px`,
-    top: `${y / 40}px`
+  $('body').mousemove(e => {
+    const x = e.offsetX
+    const y = e.offsetY
+    const circle = $('.events-list > .ono-animatedbtn__circle')
+    if (!isSmall) {
+      circle.css({
+        left: `${x / (window.innerWidth / 100)}px`,
+        top: `${y / 40}px`
+      })
+    }
   })
 })
