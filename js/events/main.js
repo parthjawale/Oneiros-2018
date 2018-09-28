e84f4a8480a9f95919ae4a5186afc57d2fec8260
parthjawale <parth.169105122@muj.manipal.edu>
2018-09-28 19:00:07 +0530

    Changes

--- js/events/main.js
+++ js/events/main.js
@@ -2,6 +2,7 @@ const item = $(".clubs-grid__item");
 
 $(document).ready(function() {
   $("#fullpage").fullpage({
+    anchors: ["main", "club-names"],
     afterLoad: (i, path) => {
       if (path.index === 0) startVideo();
     }
