78c6dba6b81f84a268bd829fadc7d1bfe73d4dc7
parthjawale <parth.169105122@muj.manipal.edu>
2018-10-01 04:17:41 +0530

    changes

--- js/config/login/index.js
+++ js/config/login/index.js
@@ -1,41 +1,53 @@
-  new Vue({
-  el: '#login',
+new Vue({
+  el: "#login",
   data: {
-    username: '',
-    password: ''
+    username: "",
+    password: ""
   },
   methods: {
-    getArray (array) {
+    getArray(array) {
       if (array) {
-        const arr = []
+        const arr = [];
         for (const key in array.docs) {
-          arr.push({ key: array.docs[key].data(),
+          arr.push({
+            key: array.docs[key].data(),
             id: array.docs[key].id
-          })
+          });
         }
-        return arr
+        return arr;
       }
     },
-    login () {
-      if (this.username != '' && this.password != '') {
-        var self = this
-        var email
-        firebase.firestore().collection('users').where('username', '==', self.username)
-        .get()
-        .then(function (querySnapshot) {
-          querySnapshot.forEach(function (doc) {
-            email = doc.data().email
-          })
-          firebase.auth().signInWithEmailAndPassword(email,self.password)
-          .then(function (user) {
-            alert('Successful Login')
-          }, function (error) {
-            alert(error.message);
-          })
-        }, function (error) {
-            alert(error.message)
-        })
+    login() {
+      if (this.username != "" && this.password != "") {
+        var self = this;
+        var email;
+        firebase
+          .firestore()
+          .collection("users")
+          .where("username", "==", self.username)
+          .get()
+          .then(
+            function(querySnapshot) {
+              querySnapshot.forEach(function(doc) {
+                email = doc.data().email;
+              });
+              firebase
+                .auth()
+                .signInWithEmailAndPassword(email, self.password)
+                .then(
+                  function(user) {
+                    window.location = "/eventregistrations";
+                  },
+                  function(error) {
+                    alert(error.message);
+                  }
+                );
+            },
+            function(error) {
+              alert(error.message);
+            }
+          );
       }
     }
   }
-})
+});
