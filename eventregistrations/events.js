e1e89557e0f85ffbf45288af78e3fd2d8b74c2c9
parthjawale <parth.169105122@muj.manipal.edu>
2018-10-03 16:44:35 +0530

    changes

--- eventregistrations/events.js
+++ eventregistrations/events.js
@@ -1,393 +1,406 @@
-var clubs = [{
-    name: 'Aperture',
-    desc: 'Photography Events',
-    events: [{
-        name: 'FOCUS',
-        price: 'Free',
-        type: 'solo'
+var clubs = [
+  {
+    name: "Aperture",
+    desc: "Photography Events",
+    events: [
+      {
+        name: "FOCUS",
+        price: "Free",
+        type: "solo"
       },
       {
-        name: 'Shutter Up',
+        name: "Shutter Up",
         price: 200,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'InstAperture',
+        name: "InstAperture",
         price: 100,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'Showdown Of Societies',
+        name: "Showdown Of Societies",
         price: 500,
         min: 3,
         max: 5,
-        type: 'fixed'
+        type: "fixed"
       },
       {
-        name: 'Picture Perfect',
+        name: "Picture Perfect",
         price: 100,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'Power Shoot',
+        name: "Power Shoot",
         price: 100,
         min: 2,
         max: 2,
-        type: 'duet'
+        type: "duet"
       }
     ]
   },
   {
-    name: 'The Music Club',
-    desc: 'Music Events',
-    events: [{
-        name: 'Octaves',
+    name: "The Music Club",
+    desc: "Music Events",
+    events: [
+      {
+        name: "Octaves",
         price: 150,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'Dhwani',
+        name: "Dhwani",
         price: 150,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'BeatStreet',
+        name: "BeatStreet",
         price: 150,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'Saptak',
+        name: "Saptak",
         price: 150,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'Ensemble',
+        name: "Ensemble",
         price: 800,
         min: 7,
         max: 1000,
-        type: 'team'
+        type: "team"
       },
       {
-        name: 'Twice The Voice',
+        name: "Twice The Voice",
         price: 250,
         min: 2,
         max: 2,
-        type: 'duet'
+        type: "duet"
       },
       {
-        name: 'Woodstock',
+        name: "Woodstock",
         price: 150,
         min: 1,
         max: 3,
-        type: 'team'
+        type: "team"
       }
     ]
   },
   {
-    name: 'Litmus',
-    desc: 'English Literature Events',
-    events: [{
-        name: 'Bamboozled',
+    name: "Litmus",
+    desc: "English Literature Events",
+    events: [
+      {
+        name: "Bamboozled",
         price: 250,
         min: 2,
         max: 3,
-        type: 'fixed'
+        type: "fixed"
       },
       {
-        name: 'Ekphrasis',
+        name: "Ekphrasis",
         price: 100,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'Just A Minute',
+        name: "Just A Minute",
         price: 100,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'British Parliamentary Debate',
+        name: "British Parliamentary Debate",
         price: 500,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'Pictionary',
+        name: "Pictionary",
         price: 150,
         min: 2,
         max: 2,
-        type: 'duet'
+        type: "duet"
       },
       {
-        name: 'Voice Over',
+        name: "Voice Over",
         price: 150,
         min: 2,
         max: 2,
-        type: 'duet'
+        type: "duet"
       }
     ]
   },
   {
-    name: 'Coreografia',
-    desc: 'Dance Events',
-    events: [{
-        name: 'NEXTARs',
+    name: "Coreografia",
+    desc: "Dance Events",
+    events: [
+      {
+        name: "NEXTAR",
         price: 200,
         min: 1,
         max: 2,
-        type: 'team'
+        type: "team"
       },
       {
-        name: 'Showcase',
+        name: "Showcase",
         price: 100,
         min: 1,
         max: 2,
-        type: 'team'
+        type: "team"
       },
       {
-        name: 'Ground Zero',
+        name: "Ground Zero",
         price: 800,
         min: 4,
         max: 6,
-        type: 'team'
+        type: "team"
       },
       {
-        name: 'Steps Vs Beats',
+        name: "Steps Vs Beats",
         price: 100,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       }
     ]
   },
   {
-    name: 'Cinefilia',
-    desc: 'Dramatics Events',
-    events: [{
-        name: 'AD-Mak',
+    name: "Cinefilia",
+    desc: "Dramatics Events",
+    events: [
+      {
+        name: "AD-Mak",
         price: 100,
         min: 3,
         max: 5,
-        type: 'team'
+        type: "team"
       },
       {
-        name: 'Awaaz',
+        name: "Awaaz",
         price: 100,
         min: 8,
         max: 20,
-        type: 'team'
+        type: "team"
       },
       {
-        name: 'Humor-Us',
+        name: "Humor-Us",
         price: 150,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'Mission Improvable',
+        name: "Mission Improvable",
         price: 100,
         min: 4,
         max: 5,
-        type: 'team'
+        type: "team"
       },
       {
         name: "Pandora's Box",
         price: 250,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'Rangmanch',
+        name: "Rangmanch",
         price: 100,
         min: 4,
         max: 15,
-        type: 'team'
+        type: "team"
       },
       {
-        name: 'Mono-Act',
+        name: "Mono-Act",
         price: 150,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'Spoofy Do',
+        name: "Spoofy Do",
         price: 150,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       }
     ]
   },
   {
-    name: 'Scribbles',
-    desc: 'Art & Craft Events',
-    events: [{
-        name: 'Fusionoid',
+    name: "Scribbles",
+    desc: "Art & Craft Events",
+    events: [
+      {
+        name: "Fusionoid",
         price: 100,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'Trippy Tiles',
+        name: "Trippy Tiles",
         price: 200,
         min: 1,
         max: 2,
-        type: 'team'
+        type: "team"
       },
       {
-        name: 'Comicstan',
+        name: "Comicstan",
         price: 150,
         min: 1,
         max: 2,
-        type: 'team'
+        type: "team"
       },
       {
-        name: 'Junk-O-Mania',
+        name: "Junk-O-Mania",
         price: 150,
         min: 1,
         max: 2,
-        type: 'team'
+        type: "team"
       },
       {
-        name: 'Art Gallery',
-        price: 'Free'
+        name: "Art Gallery",
+        price: "Free"
       }
     ]
   },
   {
-    name: 'Shabd',
-    desc: 'Hindi Literature Events',
-    events: [{
-      name: 'Izhar',
-      price: 50,
-      min: 1,
-      max: 1,
-      type: 'solo'
-    }]
-  },
-  {
-    name: 'Qureka',
-    desc: 'Quizzing Events',
-    events: [{
-        name: 'BizTech Quiz(Ignited Minds)',
-        price: 100,
-        min: 1,
-        max: 2,
-        type: 'team'
-      },
+    name: "Shabd",
+    desc: "Hindi Literature Events",
+    events: [
       {
-        name: 'General Quiz(Sadharan)',
-        price: 100,
+        name: "Izhar",
+        price: 50,
         min: 1,
-        max: 2,
-        type: 'team'
+        max: 1,
+        type: "solo"
       }
     ]
   },
   {
-    name: 'Sophia',
-    desc: 'Philosophy Events',
-    events: [{
+    name: "Qureka",
+    desc: "Quizzing Events",
+    events: [
+      {
+        name: "BizTech Quiz(Ignited Minds)",
+        price: 100,
+        min: 1,
+        max: 2,
+        type: "team"
+      },
+      {
+        name: "General Quiz(Sadharan)",
+        price: 100,
+        min: 1,
+        max: 2,
+        type: "team"
+      }
+    ]
+  },
+  {
+    name: "Sophia",
+    desc: "Philosophy Events",
+    events: [
+      {
         name: "Let's Tweet",
         price: 200,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'Talkathon',
+        name: "Talkathon",
         price: 200,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'Picturation',
+        name: "Picturation",
         price: 200,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       },
       {
-        name: 'Debate',
+        name: "Debate",
         price: 200,
         min: 1,
         max: 1,
-        type: 'solo'
+        type: "solo"
       }
     ]
   },
   {
-    name: 'Collab Events',
-    desc: 'Two is better than one!',
-    events: [{
-        name: 'Pop-A-Razzi(Litmus-Qureka)',
+    name: "Collab Events",
+    desc: "Two is better than one!",
+    events: [
+      {
+        name: "Pop-A-Razzi(Litmus-Qureka)",
         price: 100,
         min: 1,
         max: 2,
-        type: 'team'
+        type: "team"
       },
       {
-        name: 'Chakravyuh(Cinefilia-Shabd)',
+        name: "Chakravyuh(Cinefilia-Shabd)",
         price: 150,
         min: 2,
         max: 5,
-        type: 'team'
+        type: "team"
       },
       {
-        name: 'Song-Smith(TMC-Litmus)',
+        name: "Song-Smith(TMC-Litmus)",
         price: 200,
         min: 2,
         max: 4,
-        type: 'team'
+        type: "team"
       }
     ]
   },
   {
-    name: 'Major Events',
-    desc: 'Wait for It!',
-    events: [{
-        name: 'Requiem - War Of Bands',
+    name: "Major Events",
+    desc: "Wait for It!",
+    events: [
+      {
+        name: "Requiem - War Of Bands",
         price: 1500,
         min: 3,
         max: 8,
-        type: 'fixed'
+        type: "fixed"
       },
       {
-        name: 'Destival - Group Dance Competition',
+        name: "Destival - Group Dance Competition",
         price: 150,
         min: 12,
         max: 43,
-        type: 'team'
+        type: "team"
       },
       {
-        name: 'Kairos - Fashion Show',
+        name: "Kairos - Fashion Show",
         price: 200,
         min: 12,
         max: 22,
-        type: 'team'
+        type: "team"
       }
     ]
   }
-]
+];
