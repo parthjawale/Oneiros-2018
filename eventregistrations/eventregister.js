5fbda17580f339b3323cccc80e3290528e5f78c0
Apoorv Agarwal <aapoorv41@gmail.com>
2018-10-05 01:41:23 +0530

    major chenges to event registration page

--- eventregistrations/eventregister.js
+++ eventregistrations/eventregister.js
@@ -1,246 +1,283 @@
-new Vue({
-  el: '#app',
-  data: {
-    selectedClub: '',
-    eventName: '',
-    value: 0,
-    user: null,
-    error: false,
-    disabled: false,
-    requiem: {
-      bandname: '',
-      description: '',
-      contact: null,
-      songdew: '',
-      genre: ''
+$(document).ready(function() {
+  $('.notification__close').on('click', function() {
+    console.log('object')
+    $('.notification').removeClass('notification--show')
+  })
+
+  new Vue({
+    el: '#app',
+    data: {
+      selectedClub: '',
+      eventName: '',
+      value: 0,
+      user: null,
+      error: false,
+      disabled: false,
+      requiem: {
+        bandname: '',
+        description: '',
+        contact: null,
+        songdew: '',
+        genre: ''
+      },
+      destival: {
+        teamname: '',
+        links: ''
+      },
+      kairos: {
+        link: ''
+      },
+      message: '',
+      clubs,
+      userarr: [],
+      eventarr: [],
+      clubs,
+      email: ''
     },
-    destival: {
-      teamname: '',
-      links: ''
+    created() {
+      var self = this
+      firebase.auth().onAuthStateChanged(
+        function(user) {
+          if (user) {
+            self.user = user
+          }
+        },
+        function(error) {
+          console.log(error)
+        }
+      )
     },
-    clubs,
-    userarr: [],
-    eventarr: [],
-    clubs,
-    email: ''
-  },
-  created() {
-    var self = this
-    firebase.auth().onAuthStateChanged(
-      function(user) {
-        if (user) {
-          self.user = user
-          console.log(user)
+    mounted() {
+      var self = this
+      // $('body').click(function() {
+      //   self.authCheck()
+      // })
+      $('.notification__close').click(function() {
+        $(self.$refs.notification).removeClass('notification--show')
+      })
+    },
+    computed: {
+      amount: function() {
+        if (!this.error) {
+          if (this.eventName) {
+            if (this.eventName.name == 'Ensemble') {
+              if (this.value >= 7 && this.value <= 10) {
+                return this.eventName.price
+              } else if (this.value > 10) {
+                return this.eventName.price + (this.value - 10) * 100
+              }
+            } else if (this.eventName.type == 'team' && this.value != 0) {
+              return this.value * this.eventName.price
+            } else {
+              //return the the event's fixed price if type is : solo, duet and fixed
+              return this.eventName.price
+            }
+          } else {
+            return 0
+          }
         } else {
-          // alert('Please register yourself first.')
-          // window.location = '/register'
+          return 'Not Applicable'
         }
       },
-      function(error) {
-        console.log(error)
+      getParticipants: function() {
+        const type = this.eventName.type
+        return type === 'team' || type === 'fixed' ? true : false
+      },
+      requemSelected: function() {
+        return this.eventName.name == 'Requiem - War Of Bands'
+      },
+      destivalSelected: function() {
+        return this.eventName.name == 'Destival - Group Dance Competition'
+      },
+      kairosSelected: function() {
+        return this.eventName.name == 'Kairos - Fashion Show'
       }
-    )
-  },
-  computed: {
-    amount: function() {
-      if (!this.error) {
-        if (this.eventName) {
-          if (this.eventName.name == 'Ensemble') {
-            if (this.value >= 7 && this.value <= 10) {
-              return this.eventName.price
-            } else if (this.value > 10) {
-              return this.eventName.price + (this.value - 10) * 100
-            }
-          } else if (this.eventName.type == 'team' && this.value != 0) {
-            return this.value * this.eventName.price
+    },
+    watch: {
+      value: function() {
+        this.check()
+      }
+    },
+    methods: {
+      jsUcfirst: function(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+      },
+      changeclub() {
+        this.eventName = ''
+        this.error = false
+        this.value = 0
+      },
+      changevent() {
+        this.error = false
+        this.value = this.eventName.name != 'Ensemble' ? this.eventName.min : 7
+        this.check()
+      },
+      check() {
+        const eventType = this.eventName.type
+        const min = this.eventName.min
+        const max = this.eventName.max
+
+        if (this.eventName.name == 'Ensemble') {
+          return
+        }
+        if (eventType == 'solo') this.value = 1
+        if (eventType == 'duet') this.value = 2
+        if (eventType == 'team' || eventType == 'fixed') {
+          if (this.value >= min && this.value <= max) {
+            this.error = false
           } else {
-            //return the the event's fixed price if type is : solo, duet and fixed
-            return this.eventName.price
+            this.error = true
           }
-        } else {
-          return 0
         }
-      } else {
-        return 'Not Applicable'
-      }
-    },
-    getParticipants: function() {
-      const type = this.eventName.type
-      return type === 'team' || type === 'fixed' ? true : false
-    },
-    requemSelected: function() {
-      return this.eventName.name == 'Requiem - War Of Bands'
-    },
-    destivalSelected: function() {
-      return this.eventName.name == 'Destival - Group Dance Competition'
-    }
-  },
-  watch: {
-    value: function() {
-      this.check()
-    }
-  },
-  methods: {
-    jsUcfirst: function(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1)
-    },
-    changeclub() {
-      this.eventName = ''
-      this.error = false
-      this.value = 0
-    },
-    changevent() {
-      this.error = false
-      this.value = this.eventName.name != 'Ensemble' ? this.eventName.min : 7
-    },
-    check() {
-      const eventType = this.eventName.type
-      const min = this.eventName.min
-      const max = this.eventName.max
 
-      if (this.eventName.name == 'Ensemble') {
-        return
-      }
-      if (eventType == 'solo') this.value = 1
-      if (eventType == 'duet') this.value = 2
-      if (eventType == 'team' || eventType == 'fixed') {
-        if (this.value >= min && this.value <= max) {
-          this.error = false
-        } else {
-          this.error = true
-        }
-      }
-
-      if (this.requemSelected || this.destivalSelected) {
-        const event = this.requemSelected ? this.requiem : this.destival
-        var error = false
-        const keys = Object.keys(event)
-        keys.forEach(key => {
-          if (!event[key]) {
-            error = true
+        if (
+          this.requemSelected ||
+          this.destivalSelected ||
+          this.kairosSelected
+        ) {
+          var event
+          if (this.requemSelected) {
+            event = this.requiem
+          } else if (this.destivalSelected) {
+            event = this.destival
+          } else {
+            console.log('object')
+            event = this.kairos
           }
-        })
-        this.error = error
-        if (!(this.value >= min && this.value <= max)) {
-          this.error = true
+          var error = false
+          const keys = Object.keys(event)
+          keys.forEach(key => {
+            if (!event[key]) {
+              error = true
+            }
+          })
+          this.error = error
+          if (!(this.value >= min && this.value <= max)) {
+            this.error = true
+          }
         }
-      }
-    },
-    logout() {
-      firebase
-        .auth()
-        .signOut()
-        .then(function() {
-          window.location = '/eventregistrations.html'
-        })
-        .catch(function(error) {
-          alert(error.message)
-        })
-    },
-    submit() {
-      var self = this
-      self.disabled = true
-      var userdb = firebase.firestore().collection('users')
-      var eventdb = firebase.firestore().collection('events')
-      eventdb
-        .doc(self.eventName.name)
-        .get()
-        .then(
-          function(doc) {
-            if (doc.exists) {
-              if (doc.data().users != undefined || doc.data().users != null)
-                self.userarr = doc.data().users
-              var obj = {
-                user: self.user.uid,
-                value: self.value
-              }
-              var found = self.userarr.some(function(el) {
-                return el.user === self.user.uid
-              })
-              if (!found) {
-                self.userarr.push(obj)
-              }
-              eventdb.doc(self.eventName.name).update({
-                users: self.userarr
-              })
-              userdb
-                .doc(self.user.uid)
-                .get()
-                .then(function(doc) {
-                  if (
-                    doc.data().events != undefined ||
-                    doc.data().events != null
-                  )
-                    self.eventarr = doc.data().events
-                  if (!self.eventarr.includes(self.eventName.name))
-                    self.eventarr.push(self.eventName.name)
-                  userdb
-                    .doc(self.user.uid)
-                    .update({
-                      events: self.eventarr
-                    })
-                    .then(function() {
-                      alert(
-                        'Thank you for registering for ' +
+      },
+      logout() {
+        firebase
+          .auth()
+          .signOut()
+          .then(function() {
+            window.location = '/eventregistrations.html'
+          })
+          .catch(function(error) {
+            alert(error.message)
+          })
+      },
+      submit() {
+        var self = this
+        self.disabled = true
+        var userdb = firebase.firestore().collection('users')
+        var eventdb = firebase.firestore().collection('events')
+        eventdb
+          .doc(self.eventName.name)
+          .get()
+          .then(
+            function(doc) {
+              if (doc.exists) {
+                if (doc.data().users != undefined || doc.data().users != null)
+                  self.userarr = doc.data().users
+                var obj = {
+                  user: self.user.uid,
+                  value: self.value
+                }
+                var found = self.userarr.some(function(el) {
+                  return el.user === self.user.uid
+                })
+                if (!found) {
+                  self.userarr.push(obj)
+                }
+                eventdb.doc(self.eventName.name).update({
+                  users: self.userarr
+                })
+                userdb
+                  .doc(self.user.uid)
+                  .get()
+                  .then(function(doc) {
+                    if (
+                      doc.data().events != undefined ||
+                      doc.data().events != null
+                    )
+                      self.eventarr = doc.data().events
+                    if (!self.eventarr.includes(self.eventName.name))
+                      self.eventarr.push(self.eventName.name)
+                    userdb
+                      .doc(self.user.uid)
+                      .update({
+                        events: self.eventarr
+                      })
+                      .then(function() {
+                        self.message =
+                          'Thank you for registering for ' +
                           self.eventName.name +
                           '. Your unique code is: ' +
                           doc.data().ucode +
                           '. Please refer to the mail for further instructions.'
-                      )
-                      userdb
-                        .doc(self.user.uid)
-                        .get()
-                        .then(function(doc) {
-                          if (doc.exists) {
-                            ;(self.email = doc.data().email),
-                              (self.name = doc.data().name)
-                            body = {
-                              email: self.email,
-                              message:
-                                'Thank you for registering for ' +
-                                self.eventName.name +
-                                '. <br>Your unique code is ' +
-                                doc.data().ucode +
-                                '.<br>Payment instructions will be sent soon.',
-                              name: self.name
+
+                        self.$refs.modalToggle.checked = false
+                        $(self.$refs.notification).addClass(
+                          'notification--show'
+                        )
+
+                        userdb
+                          .doc(self.user.uid)
+                          .get()
+                          .then(function(doc) {
+                            if (doc.exists) {
+                              ;(self.email = doc.data().email),
+                                (self.name = doc.data().name)
+                              body = {
+                                email: self.email,
+                                message:
+                                  'Thank you for registering for ' +
+                                  self.eventName.name +
+                                  '. <br>Your unique code is ' +
+                                  doc.data().ucode +
+                                  '.<br>Payment instructions will be sent soon.',
+                                name: self.name
+                              }
+                              fetch('/mail/checkMail.php', {
+                                method: 'POST',
+                                headers: {
+                                  'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify(body)
+                              })
+                                .then(res => {
+                                  return res.json()
+                                })
+                                .then(response => {
+                                  if (response.code === 200) {
+                                    self.mujerror = "We'll get back to you!"
+                                  } else if (response.code === 405) {
+                                    self.mujerror = 'Fields cant be empty!'
+                                  } else if (response.code === 406) {
+                                    self.mujerror = 'Invalid E-Mail'
+                                  }
+                                  self.clear()
+                                })
+                              self.disabled = false
                             }
-                            fetch('/mail/checkMail.php', {
-                              method: 'POST',
-                              headers: {
-                                'Content-Type': 'application/json'
-                              },
-                              body: JSON.stringify(body)
-                            })
-                              .then(res => {
-                                return res.json()
-                              })
-                              .then(response => {
-                                if (response.code === 200) {
-                                  self.mujerror = "We'll get back to you!"
-                                } else if (response.code === 405) {
-                                  self.mujerror = 'Fields cant be empty!'
-                                } else if (response.code === 406) {
-                                  self.mujerror = 'Invalid E-Mail'
-                                }
-                                // self.clear();
-                              })
-                            self.disabled = false
-                          }
-                        })
-                    })
-                })
+                          })
+                      })
+                  })
+              }
+            },
+            function(error) {
+              alert(error.message)
+              self.disabled = false
             }
-          },
-          function(error) {
-            alert(error.message)
-            self.disabled = false
-          }
-        )
-    },
-    clear() {
-      self.selectedClub = ''
-      self.eventName = ''
-      self.disable = false
+          )
+      },
+      clear() {
+        this.selectedClub = ''
+        this.eventName = ''
+        this.disable = false
+      }
     }
-  }
+  })
 })
