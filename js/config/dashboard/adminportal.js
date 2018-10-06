f483a41fc641d12aad451a5ea6eb56ec981db366
Prerit <preritpat@hotmail.com>
2018-10-07 01:17:31 +0530

    added the admin portal without styles fornow

--- js/config/dashboard/adminportal.js
+++ js/config/dashboard/adminportal.js
@@ -13,62 +13,50 @@ new Vue ({
         regno:'',
         college:'',
         uid:'',
-        f:''
+        f:'',
+        disabled: false,
+        show: false,
+        msg:''
     },
     methods:{
         display(){
             var self=this
+            self.disabled = true
             var userdb=firebase.firestore().collection("users")
             var eventsdb=firebase.firestore().collection("events")
             
             userdb.where("ucode", "==", self.ucode)
             .get()
             .then(function(querySnapshot){
-                querySnapshot.forEach (function (doc) {
-                    console.log(doc.data())
-                    self.name=doc.data().name
-                    self.username=doc.data().username
-                    self.email=doc.data().email
-                    self.pno=doc.data().pno
-                    self.eventsarr = doc.data().events
-                    self.uid=doc.data().uid
-                    self.isManipal=doc.data().isManipal
-                    if(self.isManipal){
-                        self.regno=doc.data().regno
-                    }
-                    else{
-                        self.college=doc.data().college
-                    }
+                self.disabled = false
+                if (querySnapshot.size > 0) {
+                    querySnapshot.forEach (function (doc) {
+                        self.show = true
 
-                })
+                        console.log(doc.data())
+                        self.name=doc.data().name
+                        self.username=doc.data().username
+                        self.email=doc.data().email
+                        self.pno=doc.data().pno
+                        self.eventsarr = doc.data().events
+                        self.uid=doc.data().uid
+                        self.isManipal=doc.data().isManipal
+                        if(self.isManipal){
+                            self.regno=doc.data().regno
+                            self.f = 1;
+                        }
+                        else{
+                            self.college=doc.data().college
+                            self.f = 0;
+                        }
+                    })
+                } else {
+                    self.show = false
+                    self.msg="Invalid ucode"
+                }
             },function(error){
                 console.log(error.message)
             }) 
-
-
-
-            // for(i=0;i<eventsarr.length;i++){
-            //     eventsdb.doc(eventsarr[i].event)
-            //     .get()
-            //     .then(function(doc){
-                    
-            //     })
-            // }
-
-
-            // eventsdb.doc(eventsarr)
-            // .get()
-            // .then(function(doc){
-            //     self.eventusers=doc.data().users
-            //     for(i=0;i<self.eventusers.length;i++){
-            //         if(self.eventusers[i].user==self.uid){
-            //             f=1;
-            //         }
-            //     }
-            //     if(f){
-            //         eventdetailsarr[i]=self.eventusers[i]
-            //     }
-            // })
         }
     }
 })
