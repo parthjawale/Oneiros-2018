4f340ba2e2eb2e778b610cceb2c0fcbb41570f0c
parthjawale <parth.169105122@muj.manipal.edu>
2018-10-10 18:37:10 +0530

    changes

--- admin/byEvent/index.js
+++ admin/byEvent/index.js
@@ -1,28 +1,29 @@
-new Vue ({
-    el: '#admin-events',
-    data: {
-        clubs,
-        selectedClub: '',
-        selectedEvent: '',
-        usersarr:[],
-        namearr:[],
-        disabled:true
+new Vue({
+  el: "#admin-events",
+  data: {
+    clubs,
+    selectedClub: "",
+    selectedEvent: "",
+    usersarr: [],
+    namearr: [],
+    disabled: false
+  },
+  methods: {
+    changeclub() {
+      this.selectedEvent = "";
     },
-    methods: {
-        changeclub() {
-            this.selectedEvent = "";
-        },
-        display(){
-            var self=this
-            console.log(self.selectedEvent)
-            console.log(self.selectedClub)
-
-            firebase.firestore().collection("events").doc(self.selectedEvent.name)
-            .get()
-            .then(function(doc){
-                self.usersarr=doc.data().users
-                })
-            self.disabled=false
-        }
+    display() {
+      var self = this;
+      self.disabled = true;
+      firebase
+        .firestore()
+        .collection("events")
+        .doc(self.selectedEvent.name)
+        .get()
+        .then(function(doc) {
+          self.usersarr = doc.data().users;
+          self.disabled = false;
+        });
     }
-})
+  }
+});
